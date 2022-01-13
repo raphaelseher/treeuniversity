6b211db2e3657b5fef0abfd2d7550f3a4396a3f6
Raphael Seher <raphael.seher@gmail.com>
2022-01-13 09:27:58 +0100

    Add more data needef for studentcard

--- src/adapters/RegistrationData.ts
+++ src/adapters/RegistrationData.ts
@@ -2,6 +2,10 @@ interface IRegistrationData {
   firstname?: string;
   lastname?: string;
   birthDate?: string;
+  faculty?: string;
+  studySubject?: string;
+  image?: string; // base64 encoded
+  validUntil?: string; // fixed date for end of summer semester
 }
 
 export default IRegistrationData;
