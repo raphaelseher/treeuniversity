873b03911473e1d5cd4609d931efb7192465a75d
Raphael Seher <raphael.seher@gmail.com>
2022-01-13 20:31:40 +0100

    Add components and styling for sidebar

--- src/components/RegisterSidebar.tsx
+++ src/components/RegisterSidebar.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import IRegistrationData from "adapters/RegistrationData";
 import { useLocalStoredUser } from "adapters/storage";
 import StudentCard from "components/StudentCard";
+import Button from "@mui/material/Button";
+import "styles/RegisterSidebar.css";
 
 interface RegisterSidebarProps {
   registrationCode: string;
@@ -12,9 +14,20 @@ function RegisterSidebar(props: RegisterSidebarProps) {
     IRegistrationData | undefined
   >(props.registrationCode, undefined);
 
+  const buttonTitle = "Submit";
+
   return (
     <div id="sidebar">
-      <p>Sidebar</p>
+      <div className="sidebar-content">
+        <StudentCard registrationCode={props.registrationCode} />
+        <Button className="submit-button" variant="contained">
+          {buttonTitle}
+        </Button>
+      </div>
+      <hr />
+      <div className="sidebar-content">
+        <h2>Status Timeline</h2>
+      </div>
     </div>
   );
 }
