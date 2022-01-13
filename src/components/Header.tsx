bd431cad350749f56c3c70ca723d8aaed6385ca1
Raphael Seher <raphael.seher@gmail.com>
2022-01-13 18:36:51 +0100

    Clean css to be in correct filess

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 import Chip from "@mui/material/Chip";
+import "styles/Header.css";
 
 // Parameters are called Props in React.
-type HeaderProps = { registrationCode: string | undefined }
+type HeaderProps = { registrationCode: string | undefined };
 function Header(props: HeaderProps) {
-
   return (
-    <div>
+    <div id="header">
       <header>
         <h3>Treeuniversity</h3>
-        { // different way to do an "if" inside JSX
-          props.registrationCode &&
+        {
+          // different way to do an "if" inside JSX
+          props.registrationCode && (
             <div id="registration-code">
-            <Chip label= {props.registrationCode} /> 
+              <Chip label={props.registrationCode} />
             </div>
+          )
         }
       </header>
     </div>
