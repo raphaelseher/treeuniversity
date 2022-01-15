99e608908465f21e052d4f7fdf670e4ad72c98ae
Raphael Seher <raphael.seher@gmail.com>
2022-01-15 18:02:07 +0100

    Add sort & formatted time

--- src/components/StatusTimeline.tsx
+++ src/components/StatusTimeline.tsx
@@ -5,22 +5,40 @@ import { useUserDataContext } from "context/UserDataContext";
 interface StatusTimelineProps {}
 
 function StatusTimeline(props: StatusTimelineProps) {
-  const { state, dispatch } = useUserDataContext();
+  const state = useUserDataContext().state;
+
+  const formatDate = (timestamp: Date) => {
+    const now = new Date();
+    const date = new Date(timestamp);
+    const hDiff = Math.abs(now.valueOf() - date.valueOf()) / 36e5;
+
+    if (hDiff * 60 < 1) {
+      return "now";
+    } else if (hDiff < 1) {
+      // return date in minutes
+      return (hDiff * 60).toFixed(0) + " minutes";
+    }
+
+    return date.toLocaleDateString();
+  };
 
   return (
     <div id="status-timeline">
-      {state?.userData.statusMessages.map((message) => {
-        return (
-          <StatusTimelineMessage
-            data={{
-              timeLabel: message.timestamp.toString(),
-              text: message.message,
-              name: message.name,
-              isError: message.isError,
-            }}
-          />
-        );
-      })}
+      {state?.userData.statusMessages
+        .sort((lhs, rhs) => lhs.timestamp.valueOf() - rhs.timestamp.valueOf())
+        .map((message) => {
+          const dateString = formatDate(message.timestamp);
+          return (
+            <StatusTimelineMessage
+              data={{
+                timeLabel: dateString,
+                text: message.message,
+                name: message.name,
+                isError: message.isError,
+              }}
+            />
+          );
+        })}
     </div>
   );
 }
