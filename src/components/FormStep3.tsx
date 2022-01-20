77c2b395ee329b2100e535b6efc331b009577bb7
Raphael Seher <raphael.seher@gmail.com>
2022-01-20 17:22:58 +0100

    Add formstep3 with autocomplete dropdowns

---
+++ src/components/FormStep3.tsx
@@ -0,0 +1,58 @@
+import { useUserDataContext } from "context/UserDataContext";
+import FormStepTitle, { Step } from "components/FormStepTitle";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import { ActionType } from "reducer/userDataReducer";
+import "styles/FormStep3.css";
+
+function FormStep3() {
+  const { state, dispatch } = useUserDataContext();
+  const userData = state.userData;
+
+  const optionsMap = new Map<string, string[]>([
+    ["", []],
+    ["Informatics", ["Applied Informatic", "Telematic", "Business Informatic"]],
+  ]);
+
+  return (
+    <div id="step3-content">
+      <FormStepTitle step={Step.Step3} title="Step 3: Study Selection" />
+      <Autocomplete
+        disablePortal
+        value={userData?.faculty ?? ""}
+        onChange={(e, newValue) => {
+          dispatch({
+            type: ActionType.UpdateUserData,
+            payload: {
+              newData: { ...userData, faculty: newValue },
+            },
+          });
+        }}
+        options={Array.from(optionsMap.keys())}
+        renderInput={(params) => (
+          <TextField {...params} label="Faculty Selection" />
+        )}
+      />
+
+      <Autocomplete
+        disablePortal
+        disabled={userData?.faculty === undefined}
+        value={userData?.studySubject ?? ""}
+        onChange={(e, newValue) => {
+          dispatch({
+            type: ActionType.UpdateUserData,
+            payload: {
+              newData: { ...userData, studySubject: newValue },
+            },
+          });
+        }}
+        options={optionsMap.get(userData?.faculty ?? "") ?? []}
+        renderInput={(params) => (
+          <TextField {...params} label="Study Field Selection" />
+        )}
+      />
+    </div>
+  );
+}
+
+export default FormStep3;
