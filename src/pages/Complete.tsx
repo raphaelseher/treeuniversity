4d161d92f5291fafe4eaebd28ee1eae8016e230a
PhOberd <phoberdorfer@edu.aau.at>
2022-01-21 18:40:10 +0100

    Complete bis auf scrollbar fertig

--- src/pages/Complete.tsx
+++ src/pages/Complete.tsx
@@ -1,43 +1,66 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import '../styles/App.css';
 import StudentCard from 'components/StudentCard';
 import Header from "components/Header";
 import { Button } from '@mui/material';
+import "styles/Complete.css";
+import { textAlign, width } from '@mui/system';
+import { Link, useNavigate } from "react-router-dom";
 
 function Complete() {
+
+  const navigate = useNavigate();
+  const didClickFinish = () => {
+    // TODO: Generate Registration ID and save empty data in localstore for it.
+    // Send user to register page
+    navigate("/");
+  };
+
   return (
-    <div>
-      <header>
-          <div
-          style={{
-            alignContent:"center"
-          }}
-          >
+    <div className='CompleteScreen'>
           <div
           style={{
             width:400,
             height:260,
-            margin:30
-
+            margin:20
           }}
           >
             <StudentCard />
           </div>
-          <h1>Complete</h1>
+          <h1
+          style={{
+            marginTop:30
+          }}
+          >Registration Complete</h1>
+          <div
+          style={{
+            width:600,
+            textAlign:'left'
+          }}
+          >
+          <h2>Further Information</h2>
+          <p 
+          style={{
+            textAlign:"justify"
+          }}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
           </div>
-          <Link to="/App">
+
             <Button
             variant='contained'
             style={{
               height:48,
-              width:203
+              width:600,
+              marginTop:20,
+              marginBottom:50
             }}
+            onClick={() => {
+              didClickFinish();
+          }}
             >
               Finish
             </Button>
-          </Link>
-      </header>
+
     </div>
   );
 }
