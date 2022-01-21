16764581b6e5c6fa83aa1b48c9f8634fe5d5392d
PhOberd <phoberdorfer@edu.aau.at>
2022-01-21 14:33:20 +0100

    Startpage imge and components aligned

--- src/pages/Complete.tsx
+++ src/pages/Complete.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import '../styles/App.css';
+import StudentCard from 'components/StudentCard';
+import Header from "components/Header";
+import { Button } from '@mui/material';
 
 function Complete() {
   return (
     <div>
       <header>
+          <div
+          style={{
+            alignContent:"center"
+          }}
+          >
+          <div
+          style={{
+            width:400,
+            height:260,
+            margin:30
+
+          }}
+          >
+            <StudentCard />
+          </div>
           <h1>Complete</h1>
-          <Link to="/">Back</Link>
+          </div>
+          <Link to="/App">
+            <Button
+            variant='contained'
+            style={{
+              height:48,
+              width:203
+            }}
+            >
+              Finish
+            </Button>
+          </Link>
       </header>
     </div>
   );
