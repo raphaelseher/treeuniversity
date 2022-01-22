fb05dd3a29a5d04a838627afb1487ee338255d66
Raphael Seher <raphael.seher@gmail.com>
2022-01-23 00:28:19 +0100

    Change to have correct design

--- src/pages/Continue.tsx
+++ src/pages/Continue.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import '../styles/Complete.css';
+import React from "react";
 import Header from "components/Header";
 import TextField from "@mui/material/TextField";
 import { ActionType } from "reducer/userDataReducer";
 import UserDataContext, { useUserDataContext } from "context/UserDataContext";
 import Button from "@mui/material/Button";
 import { Link, useNavigate } from "react-router-dom";
-import { setConstantValue } from 'typescript';
-import { DatePicker, DesktopDatePicker, LocalizationProvider } from '@mui/lab';
-import Stack from '@mui/material/Stack';
-import DateAdapter from '@mui/lab/AdapterDayjs';
+import { setConstantValue } from "typescript";
+import { DatePicker, DesktopDatePicker, LocalizationProvider } from "@mui/lab";
+import Stack from "@mui/material/Stack";
+import DateAdapter from "@mui/lab/AdapterDayjs";
+import "styles/Continue.css";
 
 interface RegisterFormProps {}
 function Continue() {
   const { state } = useUserDataContext();
   const regCode = state.registrationCode;
   const codeRegex = new RegExp("(0-9)+");
-  let isCodeValid: boolean =
-  (codeRegex.test(regCode));
+  let isCodeValid: boolean = codeRegex.test(regCode);
 
   const navigate = useNavigate();
   const didClickResume = () => {
@@ -28,69 +27,52 @@ function Continue() {
   };
 
   return (
-    <div className='continue_page'
-      style={{textAlign:"center",marginTop:200}}
-    >
-      <header>
+    <div className="continue">
+      <Header showRegistrationCode={false} />
+      <div className="continue-scroll">
+        <div className="continue-content">
           <h1>Resume Registration</h1>
-
-        <TextField
-        variant="outlined"
-        label="Registration Code"
-        type="name"
-        placeholder="Registration Code eintragen"
-        helperText={isCodeValid ? undefined : "Registration Code invalid Format!"}
-        error={!isCodeValid}
-        style={{height:48,width:600,marginBottom:40}}
-        />
-        <div className='bdate'
-        style={{width:600}}
-        >
-      <LocalizationProvider dateAdapter={DateAdapter}>
-      <Stack spacing={3}>
-      <DesktopDatePicker
-          
+          <TextField
+            variant="outlined"
+            label="Registration Code"
+            type="name"
+            placeholder="Registration Code eintragen"
+            helperText={
+              isCodeValid ? undefined : "Registration Code invalid Format!"
+            }
+            error={!isCodeValid}
+          />
+          <LocalizationProvider dateAdapter={DateAdapter}>
+            <Stack spacing={3}>
+              <DesktopDatePicker
                 label="For desktop"
-
-                minDate={new Date('1900-01-01')}
-                onChange={(newValue) => {
-
-                }}
-                renderInput={(params) => <TextField {...params} />} value={undefined}        />
-      </Stack>
-      </LocalizationProvider>
-        </div>
-        <div>
-        <Button
+                minDate={new Date("1900-01-01")}
+                onChange={(newValue) => {}}
+                renderInput={(params) => <TextField {...params} />}
+                value={undefined}
+              />
+            </Stack>
+          </LocalizationProvider>
+          <Button
+            className="button"
             variant="contained"
-            style={{
-              height: 48,
-              width: 600,
-              marginTop:30
-            }}
             onClick={() => {
               didClickResume();
             }}
-        >
-          Resume Registration
-        </Button>
-        </div>
-        <div>
-        <Button
-            variant='outlined'
-            style={{
-              height: 48,
-              width: 600,
-              marginTop:20
-            }}
+          >
+            Resume Registration
+          </Button>
+          <Button
+            className="button"
+            variant="outlined"
             onClick={() => {
               didClickCancel();
             }}
-        >
-          Cancel
-        </Button>
+          >
+            Cancel
+          </Button>
         </div>
-      </header>
+      </div>
     </div>
   );
 }
