c25a42056e698ec1bad5f29a26247287209b5e46
Raphael Seher <raphael.seher@gmail.com>
2022-01-22 17:19:03 +0100

    Add uploadind and states

--- src/components/FormStep4.tsx
+++ src/components/FormStep4.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useUserDataContext } from "context/UserDataContext";
 import FormStepTitle from "components/FormStepTitle";
 import { Step } from "helper/progress";
@@ -14,33 +14,76 @@ function FormStep4() {
   const { state, dispatch } = useUserDataContext();
   const userData = state.userData;
   const showErrorState = Progress.showErrorState(state.userData);
-
   const [openFileSelector, { filesContent, loading, errors }] = useFilePicker({
     readAs: "DataURL",
     maxFileSize: 0.5,
     accept: [".pdf", ".png", ".jpg", ".jpeg"],
   });
 
-  let errorMessage1: string | undefined;
-  let errorMessage2: string | undefined;
+  const currentUpload = useRef<number>(0);
+
+  let currentErrorMessage: string | undefined;
   if (errors.length) {
     if (errors[0].fileSizeTooSmall) {
-      errorMessage1 = "File size is too small!";
+      currentErrorMessage = "File size is too small!";
     } else if (errors[0].fileSizeToolarge) {
-      errorMessage1 = "File size is too large!";
+      currentErrorMessage = "File size is too large!";
     } else if (errors[0].readerError) {
-      errorMessage1 = "Problem occured while reading file!";
+      currentErrorMessage = "Problem occured while reading file!";
     } else if (errors[0].maxLimitExceeded) {
-      errorMessage1 = "Too many files";
+      currentErrorMessage = "Too many files";
     } else if (errors[0].minLimitNotReached) {
-      errorMessage1 = "Not enought files";
+      currentErrorMessage = "Not enought files";
     }
   }
 
-  if (showErrorState && true) {
-    errorMessage1 = "Upload is missing!";
+  let errorMessage1: string | undefined;
+  let errorMessage2: string | undefined;
+
+  if (currentUpload.current == 0) {
+    errorMessage1 = currentErrorMessage;
+  } else {
+    errorMessage2 = currentErrorMessage;
   }
 
+  if (showErrorState && !userData.proofOfCitizinship && !errorMessage1) {
+    errorMessage1 = "Upload is missing!";
+  }
+  if (showErrorState && !userData.entranceQualification && !errorMessage2) {
+    errorMessage2 = "Upload is missing!";
+  }
+
+  useEffect(() => {
+    const base64FileName = filesContent[0]?.name;
+    if (!base64FileName) return;
+
+    if (currentUpload.current == 0) {
+      dispatch({
+        type: ActionType.UpdateUserData,
+        payload: {
+          newData: { ...userData, proofOfCitizinship: base64FileName },
+        },
+      });
+    } else {
+      dispatch({
+        type: ActionType.UpdateUserData,
+        payload: {
+          newData: { ...userData, entranceQualification: base64FileName },
+        },
+      });
+    }
+  }, [filesContent]);
+
+  const icon = userData.entranceQualification ? (
+    <img
+      width="22px"
+      height="32px"
+      className="done-icon"
+      src={process.env.PUBLIC_URL + "/image/done.svg"}
+    />
+  ) : (
+    <p></p>
+  );
   return (
     <div id="step4">
       <FormStepTitle step={Step.Step4} title="Step 4: Documents" />
@@ -54,6 +97,7 @@ function FormStep4() {
             <Button
               variant="outlined"
               onClick={() => {
+                currentUpload.current = 0;
                 openFileSelector();
               }}
             >
@@ -61,12 +105,37 @@ function FormStep4() {
             </Button>
           </div>
           <div className="column-upload">
-            {errorMessage1 && <Alert severity="error">{errorMessage1}</Alert>}
-            <Chip
-              className="rounded-chip inactive-chip"
-              label="Upload a file"
-              variant="outlined"
-            />
+            {errorMessage1 ? (
+              <Alert severity="error">{errorMessage1}</Alert>
+            ) : (
+              <Chip
+                className={
+                  "rounded-chip " +
+                  (userData.proofOfCitizinship
+                    ? "primary-chip"
+                    : "inactive-chip")
+                }
+                label={
+                  userData.proofOfCitizinship
+                    ? userData.proofOfCitizinship
+                    : "Upload a file"
+                }
+                variant="outlined"
+                onDelete={() => {}}
+                deleteIcon={
+                  userData.proofOfCitizinship ? (
+                    <img
+                      width="22px"
+                      height="22px"
+                      className="done-icon"
+                      src={process.env.PUBLIC_URL + "/image/done.svg"}
+                    />
+                  ) : (
+                    <p></p>
+                  )
+                }
+              />
+            )}
           </div>
         </div>
 
@@ -79,6 +148,7 @@ function FormStep4() {
             <Button
               variant="outlined"
               onClick={() => {
+                currentUpload.current = 1;
                 openFileSelector();
               }}
             >
@@ -86,12 +156,37 @@ function FormStep4() {
             </Button>
           </div>
           <div className="column-upload">
-            {errorMessage1 && <Alert severity="error">{errorMessage1}</Alert>}
-            <Chip
-              className="rounded-chip inactive-chip"
-              label="Upload a file"
-              variant="outlined"
-            />
+            {errorMessage2 ? (
+              <Alert severity="error">{errorMessage2}</Alert>
+            ) : (
+              <Chip
+                className={
+                  "rounded-chip " +
+                  (userData.entranceQualification
+                    ? "primary-chip"
+                    : "inactive-chip")
+                }
+                label={
+                  userData.entranceQualification
+                    ? userData.entranceQualification
+                    : "Upload a file"
+                }
+                variant="outlined"
+                onDelete={() => {}}
+                deleteIcon={
+                  userData.entranceQualification ? (
+                    <img
+                      width="22px"
+                      height="22px"
+                      className="done-icon"
+                      src={process.env.PUBLIC_URL + "/image/done.svg"}
+                    />
+                  ) : (
+                    <p></p>
+                  )
+                }
+              />
+            )}
           </div>
         </div>
       </div>
